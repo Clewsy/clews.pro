6d7aee1b60f80bd2a7fec374f2602fbab39d5b6d
clewsy <jadon@clews.pro>
2021-06-21 18:38:17 +1000

    Experimenting with fonts.

--- clews.dev/html/index.php
+++ clews.dev/html/index.php
@@ -10,7 +10,8 @@
 	<body>
 		<div id="header">
 			<a href="https://clews.pro">
-				<img id="clewsy_image" src="images/clewsy.png" title="clewsy" width="1000" />
+<!--				<img id="clewsy_image" src="images/clewsy_hacked.png" title="clewsy" width="1000" /> -->
+				<img id="clewsy_image" src="images/clewsy_cyberpunk.png" title="clewsy" width="1000" />
 			</a>
 		</div>
 		<div id="content">
