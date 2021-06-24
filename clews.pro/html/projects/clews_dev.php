8284fe418f360201015a5050ec3a9c7f2d92cd2f
clewsy <jadon@clews.pro>
2021-06-24 16:07:36 +1000

    Better results.

--- clews.pro/html/projects/clews_dev.php
+++ clews.pro/html/projects/clews_dev.php
@@ -7,7 +7,7 @@
 			<p>The .dev top-level domain was advertised at a discount, so I picked up clews.dev.  I didn't really have a purpose in mind at the time, so I just set it up with a link to clews.pro.</p>
 			<p>I have since used it to do some minor experimenting with CSS, HTML, PHP and <a href="https://inkscape.org/">Inkscape</a>.</p>
 			<p>Some simple PHP scripting randomizes the "clewsy" title which is actually a .png image of the "clewsy" text using various fonts.</p>
-			<p>The background image was inspired by similar <a href="https://duckduckgo.com/?t=ffab&q=outrun&iax=images&ia=images">"outrun" style</a> images.  I used the <a href="/images/clews_logo.png">clews.pro site logo</a> I previously developed as the sun, and I created the rest of the image vectors in Inkscape.</p>
+			<p>The background image was inspired by similar <a href="https://duckduckgo.com/?q=outrun+sunset&t=ffab&iar=images&iax=images&ia=images">"outrun" style</a> images.  I used the <a href="/images/clews_logo.png">clews.pro site logo</a> I previously developed as the sun, and I created the rest of the image vectors in Inkscape.</p>
 			<p>A single text link on the page will just take you to <a href="https://clews.pro/index.php">clews.pro</a>.</p>
 			<p>The site is hosted on the same server as <a href="https://clews.pro/index.php">clews.pro</a> using a <a href="https://hub.docker.com/_/nginx/">containerised implementation of Nginx</a>.  More info about this deployment can be seen on the <a href="/projects/clews.php">clews.pro project page</a>.</p>
 			<p><a class="align-center" href="images/clews_dev/clews_dev_02.png"><img src="images/clews_dev/clews_dev_02.png" alt="I don't know what I'm doing." width="1200px" style="border: 10px solid black; border-radius: 50px" /></a></p>
