bd26e909ea4514b257c76f6f9e77c3a984ee356e
clewsy <jadon@clews.pro>
2021-06-29 10:18:08 +1000

    Adjustments to about layout - updated clews.dev logo.

--- clews.pro/html/about.php
+++ clews.pro/html/about.php
@@ -6,13 +6,14 @@
 				<p>I like to learn things - today I'm learning how to write html and css.</p>
 				<p>This site is a work-in-progress.  It also serves as a central location for documenting some of my various projects.</p>
 				<p>If you'd like to contact me, try <img style="vertical-align:top;" src="/images/clews_email.png" \></p>
+				<br />
 				<table>
 					<tr>
 						<td><a href="https://gitlab.com/clewsy"><img class="icon" src="/images/gitlab_icon.png" title="GitLab Logo" alt="GitLab repo" /></a></td>
 						<td><a href="https://github.com/clewsy"><img class="icon"  src="/images/github_icon.png" title="GitHub Logo" alt="GitHub repo" /></a></td>
 						<td><a href="https://hub.docker.com/u/clewsy"><img class="icon"  src="/images/dockerhub_icon.png" title="DockerHub Logo" alt="DockerHub repo" /></a></td>
 						<td><a href="https://hackaday.io/Clewsy"><img class="icon"  src="/images/hackaday_icon.png" title="Hackaday Logo" alt="Hackaday.io profile" /></a></td>
-						<td><a href="https://clews.dev"><img class="icon"  src="/images/clews_logo.png" title="Clewsy Logo" alt="The other clews domain." /></a></td>
+						<td><a href="https://clews.dev"><img class="icon"  src="/images/clews_dev_logo.png" title="Clewsy Logo" alt="The other clews domain." /></a></td>
 					</tr>
 				</table>
 			</center>
