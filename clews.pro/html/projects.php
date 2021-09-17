132fbff316b0add23c2d92a5063ba8660d9d1c72
clewsy <jadon@clews.pro>
2021-09-17 15:33:01 +1000

    Monster logo and link.

--- clews.pro/html/projects.php
+++ clews.pro/html/projects.php
@@ -17,7 +17,7 @@
 					<th scope="row"><a href="/projects/clews.php">clews.pro</a></th>
 					<td><a href="/projects/clews.php"><img class="thumbnail" src="/projects/images/clews/clews_01.png" /></a></td>
 					<td>Meta.</td>
-					<td class="date">2021-06-29</td>
+					<td class="date">2021-09-17</td>
 					<td class="links">
 						<a href="https://gitlab.com/clewsy/clews.pro">gitlab</a><br />
 						<a href="/projects.php">Recursion...</a>
