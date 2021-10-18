96e5da89ad089ea6e9f737572c2e05217a5ea1f0
clewsy <jadon@clews.pro>
2021-10-18 16:13:48 +1100

    Began working on clews.tech html and css.

---
+++ clews.tech/html/antarctica.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Adventures of a met-tech in Antarctica.</title>
+		<link href="/css/style_antarctica.css" type="text/css" rel="stylesheet" />
+		<link rel="shortcut icon" href="/favicon_antarctica.ico">
+	</head>
+	<body>
+		<h1>met-tech in Antarctica</h1>
+		<a href="https://clews.tech/antarctica.php"><img id="clewsy_image" src="/images/clews_antarctica_logo.png" title="clewsy" /></a>
+		<p>This is where I'll share photos and video clips from my time at <a href="https://www.antarctica.gov.au/antarctic-operations/stations/casey/">Casey Station</a>.</p>
+		<table class="gallery">
+			<tr>
+				<td class="align-left">
+					<a href="/index.php"><img class="photo" src="/images/test_1.png" title="First test"></a>
+					<p>text text text text text text text text text text text text text text text text </p>
+				</td>
+				<td class="align-right">
+					<a href="/index.php"><img class="photo" src="/images/test_2.png" title="Second test"></a>
+					<p>lorem ipsum </p>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
