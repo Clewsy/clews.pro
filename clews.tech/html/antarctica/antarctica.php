336b51df0fc7824d79a9b173cf9f76d7bfb034c4
clewsy <jadon@clews.pro>
2022-06-17 21:56:55 +1000

    Updates and improvements.

--- clews.tech/html/antarctica/antarctica.php
+++ clews.tech/html/antarctica/antarctica.php
@@ -13,36 +13,52 @@
 		<table class="gallery">
 			<tr>
 				<td>
-					<a href="posts/20211102/20211102.html">
-						<p>2021-11-02</p>
-						<img src="posts/20211102/small_13_20211102_150355.jpg" title="Sun and trees">
-						<p>Final day before pre-departure isolation.</p>
+					<a href="posts/2021-12-01_walk_to_the_wharf/20211201.html">
+						<p>2021-12-01</p>
+						<img src="posts/2021-12-01_walk_to_the_wharf/small_20211201_03.jpg" title="Wharf">
+						<p>Walk down to the wharf.</p>
 					</a>
 				</td>
 				<td>
-					<a href="posts/20211022/20211022.html">
-						<p>2021-10-22</p>
-						<img src="posts/20211022/20211022-4.jpg" title="Fire-Fighting">
-						<p>Fire-fighting Training.</p>
+					<a href="posts/2021-11_random_shots_november/202111.html">
+						<p>2021-11</p>
+						<img src="posts/2021-11_random_shots_november/small_20211123_02.jpg" title="November">
+						<p>Some random photos taken throughout November.</p>
 					</a>
 				</td>
 			</tr>
 			<tr>
 				<td>
-					<a href="posts/20211016/20211016.html">
+					<a href="posts/2021-11-18_commute_from_hobart_to_casey/20211118.html">
+						<p>2021-11-18</p>
+						<img src="posts/2021-11-18_commute_from_hobart_to_casey/small_20211118_03.jpg" title="Commute">
+						<p>Finally travelled to station. Bus->Plane->Hagg</p>
+					</a>
+				</td>
+				<td>
+					<a href="posts/2021-10-16_nuyina_arrival/20211016.html">
 						<p>2021-10-16</p>
-						<img src="posts/20211016/small_20211016_095326.jpg" title="Nuyina">
+						<img src="posts/2021-10-16_nuyina_arrival/small_20211016_095326.jpg" title="Nuyina">
 						<p>The Nuyina arrives in Hobart.</p>
 					</a>
 				</td>
-<!--				<td>
-					<a href="posts/20211016/20211016.html">
-						<p>2021-10-16</p>
-						<img src="posts/20211016/small_20211016_095326.jpg" title="Nuyina">
-						<p>The Nuyina arrives in Hobart.</p>
+			</tr>
+			<tr>
+				<td>
+					<a href="posts/2021-10-22_fire_fighting_training/20211022.html">
+						<p>2021-10-22</p>
+						<img src="posts/2021-10-22_fire_fighting_training/small_20211022-7.jpg" title="Fire-Fighting">
+						<p>Fire-fighting Training.</p>
 					</a>
 				</td>
--->			</tr>
+				<td>
+					<a href="posts/2021-11-02_last_day_pre_iso/20211102.html">
+						<p>2021-11-02</p>
+						<img src="posts/2021-11-02_last_day_pre_iso/small_13_20211102_150355.jpg" title="Sun and trees">
+						<p>Final day before pre-departure isolation.</p>
+					</a>
+				</td>
+			</tr>
 		</table>
 	</body>
 </html>
